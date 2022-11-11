b1fb60182babb48643d27349aa0fee26457c60a6
Satender Yadav <satenderyadav@Sams-Machine.local>
2022-11-10 22:09:00 -0500

    added dialog viewer functionality

---
+++ DialogViewer/HeaderCell.swift
@@ -0,0 +1,26 @@
+//
+//  HeaderCell.swift
+//  DialogViewer
+//
+//  Created by Satender Yadav on 11/10/22.
+//
+
+import UIKit
+
+class HeaderCell: ContentCell {
+    override init(frame: CGRect) {
+            super.init(frame: frame)
+            label.backgroundColor = UIColor(red: 0.9, green: 0.9,
+                                            blue: 0.8, alpha: 1.0)
+            label.textColor = UIColor.black
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+        }
+        
+        override class func defaultFont() -> UIFont {
+            return UIFont.preferredFont(forTextStyle: UIFont.TextStyle.headline)
+        }
+    
+}
---
+++ DialogViewer/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  DialogViewer
+//
+//  Created by Satender Yadav on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController , UICollectionViewDelegateFlowLayout   {
+    private var sections = [
+        ["header": "First Witch",
+         "content" : "Hey, when will the three of us meet up later?"],
+        ["header" : "Second Witch",
+         "content" : "When everything's straightened out."],
+        ["header" : "Third Witch",
+         "content" : "That'll be just before sunset."],
+        ["header" : "First Witch",
+         "content" : "Where do you want to meet?"],
+        ["header" : "Second Witch",
+         "content" : "The dirt patch."],
+        ["header" : "Third Witch",
+         "content" : "I guess we'll see Mac there."]
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView?.register(ContentCell.self, forCellWithReuseIdentifier: "CONTENT")
+        
+        var contentInset = collectionView!.contentInset
+        contentInset.top = 20
+        collectionView!.contentInset = contentInset
+        
+        let layout = collectionView!.collectionViewLayout
+        let flow = layout as! UICollectionViewFlowLayout
+        flow.sectionInset = UIEdgeInsets(top: 10, left: 20, bottom: 30, right: 20)
+      
+    }
+    
+    func wordsInSection(section: Int) -> [String] {
+            let content = sections[section]["content"]
+            let spaces = NSCharacterSet.whitespacesAndNewlines
+            let words = content?.components(separatedBy: spaces)
+            return words!
+        }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+         return sections.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            let words = wordsInSection(section: section)
+            return words.count
+        }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+            let words = wordsInSection(section: indexPath.section)
+            
+            let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: "CONTENT", for: indexPath) as! ContentCell
+            cell.maxWidth = collectionView.bounds.size.width
+            cell.text = words[indexPath.row]
+            return cell
+        }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            let words = wordsInSection(section: indexPath.section)
+            let size = ContentCell.sizeForContentString(s: words[indexPath.row], forMaxWidth: collectionView.bounds.size.width)
+            return size
+        }
+
+
+}
+
